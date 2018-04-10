884ef4ed966e434254ce618a91a6bca03de511a8
Tyler Long <tyler4long@gmail.com>
2018-04-10 15:14:55 +0800

    Finish the first version of xml/Element

--- src/xml/Element.js
+++ src/xml/Element.js
@@ -1,11 +1,79 @@
+import * as R from 'ramda'
+
 class Element {
-  constructor (tagName) {
+  constructor (tagName, attributes, elementList) {
     this.tagName = tagName
+    this.attributes = attributes
+    this.elementList = elementList
+  }
+
+  attributesString () {
+    if (R.isNil(this.attributes) || R.isEmpty(this.attributes)) {
+      return ''
+    }
+    if (typeof this.attributes !== 'object') {
+      return ''
+    }
+    return ' ' + R.pipe(
+      R.toPairs,
+      R.map(pair => `${pair[0]}="${pair[1]}"`),
+      R.join(' ')
+    )(this.attributes)
+  }
+
+  elementListString () {
+    if (R.isNil(this.elementList) || R.isEmpty(this.elementList)) {
+      return ''
+    }
+    return R.pipe(
+      R.map(element => element.toString()),
+      R.join('')
+    )(Array.isArray(this.elementList) ? this.elementList : [this.elementList])
   }
 
   toString () {
-    return `<${this.tagName}/>`
+    return `<${this.tagName}${this.attributesString()}>${this.elementListString()}</${this.tagName}>`
   }
 }
 
 export default Element
+
+/*
+Ref: http://www.jsonml.org/
+
+element
+= '[' tag-name ',' attributes ',' element-list ']'
+| '[' tag-name ',' attributes ']'
+| '[' tag-name ',' element-list ']'
+| '[' tag-name ']'
+| string
+;
+tag-name
+= string
+;
+attributes
+= '{' attribute-list '}'
+| '{' '}'
+;
+attribute-list
+= attribute ',' attribute-list
+| attribute
+;
+attribute
+= attribute-name ':' attribute-value
+;
+attribute-name
+= string
+;
+attribute-value
+= string
+| number
+| 'true'
+| 'false'
+| 'null'
+;
+element-list
+= element ',' element-list
+| element
+;
+*/
--- test/xml/Element.spec.js
+++ test/xml/Element.spec.js
@@ -2,7 +2,17 @@
 import Element from '../../src/xml/Element'
 
 describe('XML Element', () => {
-  test('Hello world', () => {
-    expect(new Element('br').toString()).toBe('<br/>')
+  test('empty', () => {
+    expect(new Element('br').toString()).toBe('<br></br>')
+  })
+
+  test('attributes', () => {
+    expect(new Element('rect', { x: 0, y: 1, width: 2, height: 3 }).toString()).toBe('<rect x="0" y="1" width="2" height="3"></rect>')
+  })
+
+  test('children', () => {
+    expect(new Element('p', undefined, 'hello world').toString()).toBe('<p>hello world</p>')
+    expect(new Element('p', { style: 'color: red;' }, 'hello world').toString()).toBe('<p style="color: red;">hello world</p>')
+    expect(new Element('p', { style: 'color: red;' }, ['before', new Element('span', null, 'hello world'), 'after']).toString()).toBe('<p style="color: red;">before<span>hello world</span>after</p>')
   })
 })
