7026a827aaca9ef30164da36b918f95e132091b7
Tyler Long <tyler4long@gmail.com>
2018-04-16 16:22:00 +0800

    Two nested nodes

--- test/index.spec.js
+++ test/index.spec.js
@@ -268,6 +268,42 @@ describe('generate SVG', () => {
       ])
   })
 
+  test('two nested nodes', async () => {
+    const graph = {
+      id: 'root',
+      layoutOptions: { 'elk.algorithm': 'layered', 'elk.direction': 'RIGHT' },
+      width: 100,
+      height: 100,
+      children: [
+        {
+          id: 'n1',
+          children: [
+            {
+              id: 'n1-1',
+              width: 100,
+              height: 100
+            },
+            {
+              id: 'n1-2',
+              width: 100,
+              height: 100
+            }
+          ]
+        }
+      ]
+    }
+    const svg = await graph2svg(graph)
+    fs.writeFileSync(path.join(__dirname, 'output', 'two-nested-nodes.svg'), xmlFormat(svg))
+    expect(onml.parse(svg)).toEqual(
+      ['svg', { xmlns: 'http://www.w3.org/2000/svg', width: '260', height: '148' },
+        ['rect', { x: '12', y: '12', width: '236', height: '124', stroke: 'black', fill: 'white' }],
+        ['svg', { x: '12', y: '12', width: '236', height: '124' },
+          ['rect', { x: '12', y: '12', width: '100', height: '100', stroke: 'black', fill: 'white' }],
+          ['rect', { x: '124', y: '12', width: '100', height: '100', stroke: 'black', fill: 'white' }]
+        ]
+      ])
+  })
+
   // test('one node label', async () => {
   //   const graph = {
   //     id: 'root',
